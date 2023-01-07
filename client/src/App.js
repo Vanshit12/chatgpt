c360d092d323c0416de231b302294083c9dc97dd
vanshit12 <vanshit.muradh@gmail.com>
2023-01-07 21:22:06 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,137 @@
+import React, {useEffect} from 'react';
+
+function App() {
+
+  var form
+  var chatContainer
+  let loadInterval
+
+  useEffect(() => {
+    form = document.querySelector('form')
+    chatContainer = document.querySelector('#chat_container')
+    
+    form.addEventListener('submit', handleSubmit)
+    form.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+          handleSubmit(e)
+      }
+    })
+
+    },[]);
+
+  const bot = './assets/bot.svg'
+  const user = './assets/user.svg'
+
+  function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+  }
+
+  function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+function chatStripe(isAi, value, uniqueId) {
+  return (
+      `
+      <div class="wrapper ${isAi && 'ai'}">
+          <div class="chat">
+              <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? 'bot' : 'user'}" 
+                  />
+              </div>
+              <div class="message" id=${uniqueId}>${value}</div>
+          </div>
+      </div>
+  `
+  )
+}
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  // to clear the textarea input 
+  form.reset()
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId)
+
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv)
+
+  const response = await fetch('http://localhost:5000/', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          prompt: data.get('prompt')
+      })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = " "
+
+  if (response.ok) {
+      const data = await response.json();
+      const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+      typeText(messageDiv, parsedData)
+  } else {
+      const err = await response.text()
+
+      messageDiv.innerHTML = "Something went wrong"
+      alert(err)
+  }
+}
+
+
+  return (
+    <div className="App" id="app">
+        <div id="chat_container"></div>
+    <form>
+      <textarea name="prompt" rows="1" cols="1" placeholder="Ask codex..."></textarea>
+      <button type="submit"><img src="./assets/send.svg" alt="send"></img></button>
+    </form>
+    </div>
+  );
+}
+
+export default App;
